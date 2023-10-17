12c8fb11abf2251478caa37952cf275437abb9ff
thellzardi12 <130668477+thellzardi12@users.noreply.github.com>
2023-10-17 16:37:41 -0300

    - Commit Rebecca

--- Estações do ano.py
+++ Estações do ano.py
@@ -4,19 +4,22 @@
 print('\n\n--Verificador de Data:')
 
 #Entradas
+dia=int(input('Digite o dia: '))
 mes=int(input('Digite mês:'))
 
 #Processamento e Saída
-if mes== 12 or mes== 1 or mes==2:
-        print(f'Verão')
-if mes== 3 or mes==4 or mes==5:
-        print(f'Outuno')
-if mes== 6 or mes==7 or mes==8:
-        print(f'Inverno')
-if mes== 9 or mes==10 or mes==11:
-        print(f'Primavera')
+if mes== 12 and dia >= 22 or mes== 1 or mes==2 or mes==3 and dia<20:
+        print(f'Na data {dia}/{mes}/2023 estaremos na estação Verão')
+elif mes== 3 and dia >=20 or mes==4 or mes==5 or mes==6 and dia <21:
+        print(f'Na data {dia}/{mes}/2023 estaremos na estação Outono')
+elif mes== 6 and dia >=21 or mes==7 or mes==8 or mes==9 <23:
+        print(f'Na data {dia}/{mes}/2023 estaremos na estação Inverno')
+elif mes== 9 and dia >=23 or mes==10 or mes==11 or mes==12 and dia <22:
+        print(f'Na data {dia}/{mes}/2023 estaremos na estação Primavera')
 else:
-        print(f'nao tem estacao')
+        print(f'Informações inválidas')
+
+
 
 
 
